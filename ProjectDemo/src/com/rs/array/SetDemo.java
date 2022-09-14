f6bc855307ae75bcae0237dc2365c66b49056179
Rushikesh Suryawanshi <rushikeshsuryawanshi9881@gmail.com>
2022-09-14 12:09:36 +0530

    changed to iterator implementation

--- ProjectDemo/src/com/rs/array/SetDemo.java
+++ ProjectDemo/src/com/rs/array/SetDemo.java
@@ -1,6 +1,7 @@
 package com.rs.array;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class SetDemo {
 
@@ -12,9 +13,11 @@ public class SetDemo {
 		set.add(42);
 		set.add(12);
 		
-		for (Integer s: set) {
-			System.out.println(s);
-		}
+		Iterator<Integer> itr = set.iterator();
+		
+	while(itr.hasNext())
+	{
+		System.out.println(itr.next());
 	}
-
-}
+	}
+	}
